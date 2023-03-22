e7b09e69ab586d5b3df8b7bbb322a205fa451805
Antony <antony.jacques@laposte.net>
2023-03-22 08:22:08 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from '@/store/store'
+
+import './assets/main.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+//createApp(App).use(vuetify).mount('#app')
+
+
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(store)
+
+app.use(vuetify)
+
+app.mount('#app')
