cf7203a0e276728d35260e009d214658d1f30c69
Antony <antony.jacques@laposte.net>
2023-03-23 11:23:19 +0100

    crud methods

--- src/components/AdminStarItem.vue
+++ src/components/AdminStarItem.vue
@@ -3,24 +3,24 @@ import { mapState } from 'vuex';
       <tr>
         <td>
             <span v-if="!editMode">{{ star.firstname }}</span>
-            <v-text-field v-model="updatedFirstname" v-if="editMode" type="text" :value="star.firstname"></v-text-field>
+            <v-text-field v-model="updatedFirstname" v-if="editMode" type="text" :placeholder="star.firstname"></v-text-field>
         </td>
         <td>
             <span v-if="!editMode">{{ star.lastname }}</span>
-            <v-text-field v-model="updatedLastname" v-if="editMode" type="text" :value="star.lastname"></v-text-field>
+            <v-text-field v-model="updatedLastname" v-if="editMode" type="text" :placeholder="star.lastname"></v-text-field>
         </td>
         <td>
             <span v-if="!editMode">{{ star.description }}</span>
-            <v-textarea v-model="updatedDescription" v-if="editMode" type="text" :value="star.description"></v-textarea>
+            <v-textarea v-model="updatedDescription" v-if="editMode" type="text" :placeholder="star.description"></v-textarea>
         </td>
         <td>
             <span v-if="!editMode">{{ star.imageURL }}</span>
-            <v-text-field v-model="updatedImageURL" v-if="editMode" type="text" :value="star.imageURL"></v-text-field>
+            <v-text-field v-model="updatedImageURL" v-if="editMode" type="text" :placeholder="star.imageURL"></v-text-field>
         </td>
         <td><img :src="star.imageURL" alt="image preview"></td>
         <td>
             <v-btn color="warning" icon="mdi-pencil" v-if="!editMode"  @click="toggleEditMode" ></v-btn>
-            <v-btn v-if="editMode" color="success"  @click="updatedUser" >save</v-btn>
+            <v-btn v-if="editMode" color="success"  @click="updateStar" >save</v-btn>
             <v-btn color="red"  @click="removeUser"> remove <v-icon
           end
           icon="mdi-cancel"
@@ -49,9 +49,27 @@ export default {
         },
     },
     methods: {
-        toggleEditMode: function(){
+        toggleEditMode(){
         this.editMode = !this.editMode;
       },
+
+      updateStar(){
+          const updatedStar = {
+            firstname: this.updatedFirstname,
+            lastname: this.updatedLastname,
+            description: this.updatedDescription,
+            imageURL: this.updatedImageURL
+          }
+
+        console.log('update :', updatedStar)
+        this.toggleEditMode()
+        //this.saveUpdatedUser(updatedUser) wip
+      },
+
+      removeStar(){
+          this.deleteStateStar(this.star.id)
+      }
+
     }
 
 }
